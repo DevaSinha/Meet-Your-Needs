96c76b0092195ec742f68d09da1ffe31edc95e5b
Devanshu Sinha <devanshusinha17@gmail.com>
2023-02-28 09:27:58 +0530

    login

---
+++ ReactFrontEnd/meet-your-need/src/Components/login.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+
+function Form() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Username:
+        <input type="text" value={username} onChange={handleUsernameChange} />
+      </label>
+      <br />
+      <label>
+        Password:
+        <input type="password" value={password} onChange={handlePasswordChange} />
+      </label>
+      <br />
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
+
+export default Form;
